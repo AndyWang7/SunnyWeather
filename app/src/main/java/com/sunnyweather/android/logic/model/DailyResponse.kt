476b559ada54ee2595d481e957cc10b1b89e3ad6
AndyWang <643699551@qq.com>
2020-12-21 23:18:38 +0800

    P627 JSON

---
+++ app/src/main/java/com/sunnyweather/android/logic/model/DailyResponse.kt
@@ -0,0 +1,8 @@
+package com.sunnyweather.android.logic.model
+
+//data class DailyResponse(val status: String, val result: Result){
+//    data class Result(val daily: Daily)
+//    data class Daily()
+//    data class Temperature()
+//
+//}
