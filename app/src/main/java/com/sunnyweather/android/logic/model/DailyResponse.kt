c68be844ca827a056c80ddb0e031279f2ba9faf6
Andy <643699551@qq.com>
2020-12-22 17:50:30 +0800

    fit 实现搜索全球城市数据功能 P625

--- app/src/main/java/com/sunnyweather/android/logic/model/DailyResponse.kt
+++ app/src/main/java/com/sunnyweather/android/logic/model/DailyResponse.kt
@@ -1,8 +1,14 @@
 package com.sunnyweather.android.logic.model
 
-//data class DailyResponse(val status: String, val result: Result){
-//    data class Result(val daily: Daily)
-//    data class Daily()
-//    data class Temperature()
-//
-//}
+import java.util.*
+
+data class DailyResponse(val status: String, val result: Result){
+    data class Result(val daily: Daily)
+    data class Daily(val temperature: List<Temperature>, val skycon: List<Skycon>, )
+    data class Temperature(val max: Float, val min: Float)
+    data class Skycon(val value: String, val date: Date)
+    data class LifeIndex(val coldRisk: List<LifeDescription>,val carWashing: List<LifeDescription>,
+    val ultraviolet: List<LifeDescription>,val dressing: List<LifeDescription>)
+
+    data class LifeDescription(val desc: String)
+}
